3bae6e257251bb441718c768ebc7846adfda9e75
spamerspamer <45140505+spamerspamer@users.noreply.github.com>
2018-11-18 22:43:21 +0300

    Add files via upload

---
+++ Dream.js
@@ -0,0 +1,14 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("513800242370445314")
+setInterval(function() {
+channel.send(`spamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamvspamspamspamspamspamspamspamspamspamspamspamspamspamspamspam`);
+}, 25)
+})
+ 
+ 
+client.login("NTEzNzgwODc0MTk4OTA4OTM4.DtNI8Q.abM6xzac0eooqqaB333Fdn19bbo");
